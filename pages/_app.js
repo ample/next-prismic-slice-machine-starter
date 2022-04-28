c3f1a144fdeed117ea0a5fc801db11db11596b76
Jeff Lay <phritolay@users.noreply.github.com>
2022-04-28 16:15:27 -0400

    add @prismicio/next, default prismicio.js
    and update _app.js for Prismic

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Link from 'next/link';
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
+import { linkResolver, repositoryName } from '../prismicio';
+
+// ---------------------------------------------------------
 
 import '../styles/libs/sanitize.scss';
 import '../styles/global-styles.scss';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// ---------------------------------------------------------
+
+export default function App({ Component, pageProps }) {
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
