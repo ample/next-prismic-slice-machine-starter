8848525460d8ca04b52b76ac710114271766e987
Jeff Lay <phritolay@users.noreply.github.com>
2022-05-15 22:57:55 -0400

    add base LinkList component
    resolves #11

---
+++ components/link-list/stories.js
@@ -0,0 +1,18 @@
+import { fixtures, component as LinkList } from '.';
+
+// ---------------------------------------------------------
+
+export default {
+  component: LinkList,
+  parameters: {
+    chromatic: { disableSnapshot: true },
+  },
+  title: 'Components/LinkList',
+};
+
+const Template = (args) => <LinkList {...args} />;
+
+// ---------------------------------------------------------
+
+export const _Props = Template.bind({});
+_Props.args = { ...fixtures._props };
