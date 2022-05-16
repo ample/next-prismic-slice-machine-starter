87ed2eaccc1fae805d7978c0f82c22c71e886a8b
Jeff Lay <phritolay@users.noreply.github.com>
2022-05-15 22:57:55 -0400

    add base LinkList component
    resolves #11

---
+++ components/link-list/fixtures.js
@@ -0,0 +1,10 @@
+export default {
+  _props: {
+    links: [
+      { label: 'Vestibulum vitae', url: '#' },
+      { label: 'Pellentesque tempor venenatis', url: '#' },
+      { label: 'Fusce molestie', url: '#' },
+      { label: 'Etiam in lacinia dolor', url: '#' },
+    ],
+  },
+};
