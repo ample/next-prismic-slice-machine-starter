28134d4674c3e35ea0c5e2ae9be77673b21d9a81
Jeff Lay <phritolay@users.noreply.github.com>
2022-05-21 13:35:26 -0400

    update 404 page with footer content
    and make 404 conent reuseable

--- pages/404.js
+++ pages/404.js
@@ -5,16 +5,19 @@ import { createClient } from '@root/prismicio'
 // ---------------------------------------------------------
 
 import Layout from '@layout/index'
+import Link from '@components/link'
 
 // ---------------------------------------------------------
 
 export const getStaticProps = async ({ previewData }) => {
   const client = createClient({ previewData })
 
+  const footer = await client.getSingle('footer')
   const header = await client.getSingle('header')
 
   return {
     props: {
+      footer: footer.data,
       header: header.data,
     },
   }
@@ -22,13 +25,26 @@ export const getStaticProps = async ({ previewData }) => {
 
 // ---------------------------------------------------------
 
-const NotFoundPage = ({ header }) => (
-  <Layout header={header}>
+export const PageNotFoundContent = (
+  <>
     <h1>NOT FOUND</h1>
     <p>Uh oh! This page doesn't exist.</p>
-  </Layout>
+    <Link url="/">Back to the homepage?</Link>
+  </>
 )
 
 // ---------------------------------------------------------
 
-export default NotFoundPage
+const PageNotFound = (props) => {
+  let { footer, header } = props
+
+  return (
+    <Layout footer={footer} header={header}>
+      {PageNotFoundContent}
+    </Layout>
+  )
+}
+
+// ---------------------------------------------------------
+
+export default PageNotFound
