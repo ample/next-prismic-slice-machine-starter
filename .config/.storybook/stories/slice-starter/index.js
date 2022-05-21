6e1cc9a971d0038fbe6fddd7bc4707f30bfee5cb
Jeff Lay <phritolay@users.noreply.github.com>
2022-05-21 08:53:24 -0400

    update component starters to use named export
    for better aliasing support in vs code

--- .config/.storybook/stories/slice-starter/index.js
+++ .config/.storybook/stories/slice-starter/index.js
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------
 
-import component from './component'
+import SliceName from './component'
 
 // ---------------------------------------------------------
 
-export default component
+export default SliceName
