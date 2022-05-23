d872864a9538688ab05a041639e2c139b8413b44
Jeff Lay <phritolay@users.noreply.github.com>
2022-05-23 12:51:27 -0400

    add viewport meta tag

--- layout/index.js
+++ layout/index.js
@@ -29,7 +29,8 @@ const Layout = (props) => {
   return (
     <>
       <Head>
-        <meta content={themeColor} name="theme-color"></meta>
+        <meta content={themeColor} name="theme-color" />
+        <meta content="initial-scale=1.0, width=device-width" name="viewport" />
         <link href="/favicons/favicon-48.png" rel="icon" type="image/png" />
         <link href="/favicons/favicon.svg" rel="icon" type="image/svg+xml" />
         <link href="/favicons/favicon-apple-touch.png" rel="apple-touch-icon" />
