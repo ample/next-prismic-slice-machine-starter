2f9658acb2cc88990efc071949cbbdf473f7a5b4
Jeff Lay <phritolay@users.noreply.github.com>
2022-06-17 01:48:44 -0400

    update storyshots with asyncJest
    for react lazy imports

--- storyshots.test.js
+++ storyshots.test.js
@@ -1,11 +1,37 @@
 // ---------------------------------------------------------
 
 import initStoryshots, {
-  multiSnapshotWithOptions,
+  Stories2SnapsConverter,
 } from '@storybook/addon-storyshots'
+import wait from 'waait'
+import { act, create } from 'react-test-renderer'
+import { createElement } from 'react'
+
+// ---------------------------------------------------------
+
+const converter = new Stories2SnapsConverter()
+
+// ---------------------------------------------------------
 
 initStoryshots({
+  asyncJest: true,
   configPath: './.config/.storybook',
   integrityOptions: { cwd: __dirname },
-  test: multiSnapshotWithOptions(),
+  test: async ({ context, done, story }) => {
+    let renderer
+    act(() => {
+      renderer = create(createElement(story.render))
+    })
+
+    // -------------------------------------------------------
+
+    await act(() => wait(0))
+
+    // -------------------------------------------------------
+
+    const snapshotFileName = converter.getSnapshotFileName(context)
+    expect(renderer).toMatchSpecificSnapshot(snapshotFileName)
+
+    done()
+  },
 })
