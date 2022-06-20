c3fb4aacc7949f11d0bd5575040750d0f5b76296
Jeff Lay <phritolay@users.noreply.github.com>
2022-06-20 10:37:33 -0400

    add preconnect links in layout

--- layout/index.js
+++ layout/index.js
@@ -37,6 +37,10 @@ const Layout = (props) => {
       <Head>
         <meta content={themeColor} name="theme-color" />
         <meta content="initial-scale=1.0, width=device-width" name="viewport" />
+
+        <link href="https://ample-prismic-demo.prismic.io" rel="preconnect" />
+        <link href="https://static.cdn.prismic.io" rel="preconnect" />
+
         <link href="/favicons/favicon-48.png" rel="icon" type="image/png" />
         <link href="/favicons/favicon.svg" rel="icon" type="image/svg+xml" />
         <link href="/favicons/favicon-apple-touch.png" rel="apple-touch-icon" />
