bd39e701e34c57af7c02d5f10aa093cefc800bcb
Jeff Lay <phritolay@users.noreply.github.com>
2022-08-11 14:43:05 -0400

    update next.config.js to fix warning
    for Invalid options detected

--- next.config.js
+++ next.config.js
@@ -20,7 +20,6 @@ module.exports = {
   publicRuntimeConfig: {
     DEBUG_MEDIA_QUERIES: process.env.DEBUG_MEDIA_QUERIES,
   },
-  requireConfigFile: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@use 'global' as *;`,
